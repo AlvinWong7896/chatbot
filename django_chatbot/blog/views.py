8dcc1f6a9477c0a2cc1e6de9dace1cfcc50d526b
CK Wong <alvinwong7896@gmail.com>
2023-12-20 18:45:25 +0800

    completed part 1-14

---
+++ django_chatbot/blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("This is my first url")
+
+
+def specific(request):
+    return HttpResponse("This is the specific url")
+
+
+def article(request, article_id):
+    return render(request, "blog/index.html", {"article_id": article_id})
