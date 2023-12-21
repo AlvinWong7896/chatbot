e48e2a2af2f59ccdf86ea77fc111c9309a9009b6
CK Wong <alvinwong7896@gmail.com>
2023-12-21 10:55:30 +0800

    simple bot ui

--- django_chatbot/blog/urls.py
+++ django_chatbot/blog/urls.py
@@ -6,5 +6,4 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("specific", views.specific, name="specific"),
-    path("article/<int:article_id>", views.article, name="article"),
 ]
