0e9a6e1017ff750dbdccb9e1f44554eca9467f3b
CK Wong <alvinwong7896@gmail.com>
2023-12-22 09:41:29 +0800

    part 27: start training using chatterbot but unable to load imported apps

--- django_chatbot/blog/views.py
+++ django_chatbot/blog/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+bot = ChatBot("chatbot", read_only=False, logic_adapters=["chatterbot.logic.BestMatch"])
+
+list_to_train = [
+    "hi",
+    "hi, there",
+    "What's your name?",
+    "I'm just a chatbot",
+    "What is your favorable food?",
+    "I like cheese",
+]
+
+list_trainer = ListTrainer(bot)
+list_trainer.train(list_to_train)
+
 
 def index(request):
     return render(request, "blog/index.html")
@@ -8,3 +25,8 @@ def index(request):
 
 def specific(request):
     return HttpResponse("list1")
+
+
+def getResponse(request):
+    userMessage = request.GET.get("userMessage")
+    return HttpResponse(userMessage)
